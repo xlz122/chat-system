b7256328b8fc3b54fbcdfe7070bef0d74bbf808d
xlz122 <122874722@qq.com>
2021-05-14 16:18:00 +0800

    feat: 登录静态页面，待完善

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import LoginForm from './LoginForm';
 import './login.scss';
 
 function Login(): React.ReactElement {
   return (
     <div className="login">
-      login
+      <LoginForm />
     </div>
   );
 }
