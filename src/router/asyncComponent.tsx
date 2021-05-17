1fe73ad12fade458377e53d01872a47f950e0433
xlz122 <122874722@qq.com>
2021-05-17 16:41:36 +0800

    fix: 部分文件缺少类型补充

--- src/router/asyncComponent.tsx
+++ src/router/asyncComponent.tsx
@@ -1,9 +1,17 @@
-// @ts-nocheck
 import React, { Component } from 'react';
+import { Props, State } from '@/type/index';
 
-export default function asyncComponent(importComponent) {
-  class AsyncComponent extends Component {
-    constructor(props) {
+// component 类型暂未完成
+type IState = State & {
+  // component: React.ReactElement | null;
+  component: any;
+}
+
+type ImportComponent = () => Promise<{ default: React.ReactElement }>;
+
+export default function asyncComponent(importComponent: ImportComponent) {
+  class AsyncComponent extends Component<Props, IState> {
+    constructor(props: Props) {
       super(props);
       this.state = {
         component: null
