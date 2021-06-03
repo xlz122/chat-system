22c32a570db41ee9e6b3805a1a66181b3bd1abfc
xlz122 <122874722@qq.com>
2021-06-03 15:21:15 +0800

    feat: axios加入ts类型，代码格式整理

--- src/views/more/index.tsx
+++ src/views/more/index.tsx
@@ -1,34 +1,34 @@
-/*
- * @Descripttion: 
- * @version: 
- * @Author: Qyc
- * @Date: 2021-05-17 15:46:07
- * @LastEditors: Qyc
- * @LastEditTime: 2021-05-18 09:57:10
- */
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-import React, { useState,useEffect } from 'react';
-import { useDispatch,useSelector } from 'react-redux';
-import {useHistory} from 'react-router-dom';
 function MoreBody(): React.ReactElement {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const routeName = useSelector((state: { routeName: string; }) => state.routeName);
-   
-    console.log(history); 
-       
-    const changeRoute = () => {
-        let name  = 'message';
-        dispatch({type:'ROUTENAME',data:name});
-        console.log(routeName);
-        history.push(name);
-      };
-    return (
-      <div className="home">
-        <button onClick={() => {changeRoute();}}>message</button>
-        MoreBody
-      </div>
-    );
-  }
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const routeName = useSelector(
+    (state: { routeName: string }) => state.routeName
+  );
 
-  export default MoreBody;
+  console.log(history);
+
+  const changeRoute = () => {
+    let name = 'message';
+    dispatch({ type: 'ROUTENAME', data: name });
+    console.log(routeName);
+    history.push(name);
+  };
+  return (
+    <div className="home">
+      <button
+        onClick={() => {
+          changeRoute();
+        }}
+      >
+        message
+      </button>
+      <p>MoreBody</p>
+    </div>
+  );
+}
+
+export default MoreBody;
